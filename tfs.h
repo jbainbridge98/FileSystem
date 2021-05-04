ae4b8f77400a751674951e731680550c27b15add
Jackson Bainbridge <56710599+jbainbridge98@users.noreply.github.com>
2021-05-04 16:58:27 -0400

    PUSH 5/4

--- tfs.h
+++ tfs.h
@@ -16,8 +16,7 @@
 #define MAGIC_NUM 0x5C3A
 #define MAX_INUM 1024
 #define MAX_DNUM 16384
-
-struct superblock *sb;
+#define ROOT_INUM 1
 
 
 struct superblock {
@@ -48,6 +47,8 @@ struct dirent {
 	uint16_t len;					/* length of name */
 };
 
+struct superblock *sb;
+struct inode *inodes[MAX_INUM+1];
 
 /*
  * bitmap operations
